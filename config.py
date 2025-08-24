7e32682d2fd2b673fa32c0d1a6ed6dcc1da31381
gajendrajangid83 <saraswatisharmapbc@gmail.com>
2025-08-24 18:23:06 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+
+class Config(object):
+    BOT_TOKEN = "7472777466:AAHdCsYtZpPnoMv67OL5_Hv2yjvy2Nz8jEg"
+    API_ID = 22581733
+    API_HASH = "1db7bdcf908100cc641c6a5276765c3d"
+    ADMIN = '6530997270'.split(',')
+    ADMIN_ID = [int(id) for id in ADMIN]
+    DB_URL = "mongodb+srv://neoayush444:3kuwGf9jKicbDxvT@cluster0.f9vq7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    DB_NAME = "Crack_Jack"
+    TXT_LOG = -1002294532244
+    AUTH_LOG = -1002294532244
+    HIT_LOG = -1002294532244
+    DRM_DUMP = -1002294532244
+    CHANNEL = --1002487777733
+    CH_URL = "https://t.me/justfortestinggwak"
+    OWNER = "https://t.me/mrcraacker"
+    THUMB_URL = "https://vault.pictures/media/images/b9/ad/12/b9ad123d95cd49465fcb954d.jpg" #Replace by with your Thumb URL
+    HOST = "https://drm-api-five.vercel.app"
